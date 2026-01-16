f8d46628aa11ee8a488127eb0036056ee67e34fa
Matti Krebelder <matti.krebelder@gmx.de>
2026-01-16 22:31:00 +0100

    Update version from 4.1 to 4.2

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 app.secret_key = 'your-secret-key'
 
-VERSION = "4.1"
+VERSION = "4.2"
 LATEST_VERSION = None
 UPDATE_AVAILABLE = False
 
